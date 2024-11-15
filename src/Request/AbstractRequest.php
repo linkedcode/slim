4fc6692061738d611ff375deeba2aee130b1084b
Nicolas H. Kosciuk <nhk@linkedcode.com>
2024-11-15 20:03:47 -0300

    Request

--- src/Request/AbstractRequest.php
+++ src/Request/AbstractRequest.php
@@ -2,58 +2,13 @@
 
 namespace Linkedcode\Slim\Request;
 
-use DomainException;
-use Exception;
-use Linkedcode\Slim\Exception\ValidationException;
 use Psr\Http\Message\ServerRequestInterface;
 
 abstract class AbstractRequest
 {
-    private const TYPE_STRING = 'string';
-    public const TYPE_INT = 'integer';
-    public const TYPE_FLOAT = 'float';
+    protected function __construct() {}
 
-    protected const RULES = array();
-
-    public static function get(array $data, string $attribute, string $type, string|int|float $default)
-    {
-        switch ($type) {
-            case self::TYPE_STRING:
-                if (!isset($data[$attribute])) {
-                    $value = $default;
-                } else if (empty($data[$attribute])) {
-                    $value = $default;
-                } else {
-                    $value = (string) $data[$attribute];
-                }
-
-                return $value;
-            case self::TYPE_INT:
-                if (!isset($data[$attribute])) {
-                    $value = $default;
-                } else if ($data[$attribute] === "") {
-                    $value = $default;
-                } else {
-                    $value = (int) $data[$attribute];
-                }
-
-                return $value;
-            case self::TYPE_FLOAT:
-                if (!isset($data[$attribute])) {
-                    $value = $default;
-                } else if ($data[$attribute] === "") {
-                    $value = $default;
-                } else {
-                    $value = (int) $data[$attribute];
-                }
-
-                return $value;
-            default:
-                throw new Exception("Falta programar tipo para request");
-        }
-    }
-
-    public static function createFromRequest(ServerRequestInterface $request): static
+    public static function fromRequest(ServerRequestInterface $request): static
     {
         return static::fromArray($request->getParsedBody());
     }
@@ -70,47 +25,4 @@ abstract class AbstractRequest
 
         return $request;
     }
-
-    /*protected static function validate(array $data): array
-    {
-        //$newdata = [];
-        $invalidParams = [];
-
-        foreach (static::RULES as $field => $fieldRules) {
-            try {
-                $value = null;
-
-                if (isset($data[$field])) {
-                    $value = $data[$field];
-                }
-
-                foreach ($fieldRules as $rule => $ruleValue) {
-                    switch ($rule) {
-                        case 'required':
-                            $data[$field] = static::validateRequired($value);
-                            break;
-                        default:
-                            throw new Exception("Falta programar");
-                    }
-                }
-            } catch (DomainException $e) {
-                $invalidParams[] = array('name' => $field, 'reason' => $e->getMessage());
-            }
-        }
-
-        if (false === empty($invalidParams)) {
-            throw new ValidationException("Errores de validacion", $invalidParams);
-        }
-
-        return $data;
-    }
-
-    private static function validateRequired(mixed $value)
-    {
-        if (is_null($value) || $value === "") {
-            throw new Exception("El valor es requerido.");
-        }
-
-        return $value;
-    }*/
 }
--- src/Request/RequestValidator.php
+++ src/Request/RequestValidator.php
@@ -7,39 +7,60 @@ use Linkedcode\Slim\Exception\ValidationException;
 
 class RequestValidator
 {
-    private array $invalidParams;
+    protected const MIN_LENGTH = 'minLength';
+    protected const MAX_LENGTH = 'maxLength';
+    protected const REQUIRED = 'required';
+    protected const MAXIMUM = 'maximum';
+    protected const MINIMUM = 'minimum';
+    protected const TYPE = 'type';
 
-    private array $rules;
+    protected const TYPE_STRING = 'string';
+    protected const TYPE_INTEGER = 'integer';
+    protected const TYPE_NUMBER = 'number'; // float
+
+    protected array $invalidParams = [];
+
+    protected array $rules = [];
 
     public function validate(array $data): array
     {
-        $newdata = [];
-
         foreach ($this->rules as $field => $fieldRules) {
-            $newdata[$field] = $this->validateField($field, $fieldRules, $data);
+            $data[$field] = $this->validateField($field, $fieldRules, $data);
         }
 
         $this->checkViolations();
 
-        return $newdata;
+        return $data;
     }
 
     private function validateField(string $field, array $rules, array $data)
     {
-        $value = null;
-
         if (isset($data[$field])) {
             $value = $data[$field];
+        } else {
+            $value = $this->getEmptyValue($rules[self::TYPE]);
         }
 
         foreach ($rules as $rule => $ruleValue) {
             switch ($rule) {
-                case 'required': 
-                    return $this->validateRequired($field, $value);
+                case self::REQUIRED:
+                    $this->validateRequired($field, $value);
+                    break;
+                case self::MIN_LENGTH:
+                    $this->validateMinLength($field, $value, $ruleValue);
+                    break;
+                case self::MAX_LENGTH:
+                    $this->validateMaxLength($field, $value, $ruleValue);
+                    break;
+                case self::TYPE:
+                    $value = $this->validateType($field, $value, $ruleValue);
+                    break;
                 default:
-                    throw new Exception("Falta programar");
+                    throw new Exception("Falta programar `{$rule}`");
             }
         }
+
+        return $value;
     }
 
     private function validateRequired(string $field, mixed $value): mixed
@@ -51,15 +72,59 @@ class RequestValidator
         return $value;
     }
 
-    private function addInvalidParam(string $name, string $reason)
+    private function validateMinLength(string $field, mixed $value, int $minLength): mixed
     {
-        $this->invalidParams = array('name' => $name, 'reason' => $reason);
+        if (strlen($value) < $minLength) {
+            $this->addInvalidParam($field, "La longitud mínima es de {$minLength} caracteres.");
+        }
+
+        return $value;
+    }
+
+    private function validateMaxLength(string $field, mixed $value, int $maxLength): mixed
+    {
+        if (strlen($value) > $maxLength) {
+            $this->addInvalidParam($field, "La longitud máxima es de {$maxLength} caracteres.");
+        }
+
+        return $value;
+    }
+
+    private function validateType(string $field, mixed $value, string $type): mixed
+    {
+        switch ($type) {
+            case self::TYPE_INTEGER:
+                return intval($value);
+            case self::TYPE_NUMBER:
+                return floatval($value);
+            case self::TYPE_STRING:
+                return (string) $value;
+        }
+
+        return $value;
+    }
+
+    private function addInvalidParam(string $name, string $reason): void
+    {
+        $this->invalidParams[] = array('name' => $name, 'reason' => $reason);
     }
 
     private function checkViolations()
     {
         if (false === empty($this->invalidParams)) {
             throw new ValidationException("Errores de validacion", $this->invalidParams);
+        }
+    }
+
+    private function getEmptyValue(string $type): mixed
+    {
+        switch ($type) {
+            case self::TYPE_INTEGER:
+                return 0;
+            case self::TYPE_NUMBER:
+                return 0.0;
+            case self::TYPE_STRING:
+                return "";
         }
     }
 }
