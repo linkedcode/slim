124a539550c05f2511ce51dca19f783177df35d1
Nicolas H. Kosciuk <nhk@linkedcode.com>
2024-12-19 23:24:21 -0300

    Crear cuenta al verificar

--- src/Action/Auth/LoginAction.php
+++ src/Action/Auth/LoginAction.php
@@ -2,15 +2,8 @@
 
 namespace Linkedcode\Slim\Action\Auth;
 
-use App\Infrastructure\Persistence\UserRepository;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Exception;
-use Lcobucci\JWT\Configuration;
-use Lcobucci\JWT\Signer\Key\InMemory;
-use Lcobucci\JWT\Signer\Key\LocalFileReference;
-use Lcobucci\JWT\Signer\Rsa\Sha256;
-use Lcobucci\JWT\Token;
 use Linkedcode\Slim\Settings;
 
 class LoginAction 
@@ -19,9 +12,7 @@ class LoginAction
 
     const ACCESS_TOKEN_URL = "auth/token";
 
-    private UserRepository $userRepository;
-
-    public function __construct(UserRepository $userRepository, Settings $settings)
+    public function __construct(Settings $settings)
     {
         $this->userRepository = $userRepository;
         $this->settings = $settings;
@@ -44,46 +35,11 @@ class LoginAction
         $res = $this->post($this->getAuthUrl(self::ACCESS_TOKEN_URL), $body, $headers);
 
         if ($this->curlInfo['http_code'] == "200") {
-            $this->createUser($res);
             $response->getBody()->write($res);
             return $response->withHeader('Content-Type', 'application/json');
         } else {
             $response->getBody()->write($res);
             return $response->withHeader('Content-Type', 'application/json')->withStatus($this->curlInfo['http_code']);
-        }
-    }
-
-    /**
-     * @return Token
-     */
-    protected function parseToken($jwt) : Token
-    {
-        $key = $this->settings->getPublicKey();
-
-        $config = Configuration::forAsymmetricSigner(
-            new Sha256(),
-            InMemory::file($key),
-            InMemory::base64Encoded('mBC5v1sOKVvbdEitdSBenu59nfNfhwkedkJVNabosTw=')
-        );
-
-        $arr = json_decode($jwt);
-        $token = $config->parser()->parse($arr->access_token);
-        return $token;
-    }
-
-    public function createUser($accessToken)
-    {
-        try {
-            $token = $this->parseToken($accessToken);
-            $claims = $token->claims();
-            $id = (int) $claims->get('sub');
-            $user = $this->userRepository->find($id);
-            if (!$user) {
-                $this->userRepository->createUserFromId($id);
-                return true;
-            }
-        } catch (Exception $e) {
-            $this->createUser($accessToken);
         }
     }
 }
--- src/Action/Auth/RegisterVerifyAction.php
+++ src/Action/Auth/RegisterVerifyAction.php
@@ -2,16 +2,25 @@
 
 namespace Linkedcode\Slim\Action\Auth;
 
+use App\Infrastructure\Persistence\UserRepository;
 use Linkedcode\Slim\Settings;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Exception;
+use Lcobucci\JWT\Configuration;
+use Lcobucci\JWT\Signer\Key\InMemory;
+use Lcobucci\JWT\Signer\Rsa\Sha256;
+use Lcobucci\JWT\Token;
 
 class RegisterVerifyAction
 {
     use CommonTrait;
 
-    public function __construct(Settings $settings)
+    private UserRepository $userRepository;
+
+    public function __construct(UserRepository $userRepository, Settings $settings)
     {
+        $this->userRepository = $userRepository;
         $this->settings = $settings;
     }
 
@@ -36,12 +45,51 @@ class RegisterVerifyAction
             $body['client_secret'] = 'secret';
     
             $res = $this->post($this->getAuthUrl("auth/token"), $body, $headers);
+
+            $token = $this->parseToken($res);
+            if ($token instanceof Token) {
+                $this->createUser($res);
+            }
     
             $response->getBody()->write($res);
             return $response->withHeader('Content-Type', 'application/json');
         } else {
             $response->getBody()->write($res);
             return $response->withHeader('Content-Type', 'application/json')->withStatus($this->curlInfo['http_code']);
+        }
+    }
+
+    /**
+     * @return Token
+     */
+    private function parseToken($jwt): Token
+    {
+        $key = $this->settings->getPublicKey();
+
+        $config = Configuration::forAsymmetricSigner(
+            new Sha256(),
+            InMemory::file($key),
+            InMemory::base64Encoded('mBC5v1sOKVvbdEitdSBenu59nfNfhwkedkJVNabosTw=')
+        );
+
+        $arr = json_decode($jwt);
+        $token = $config->parser()->parse($arr->access_token);
+        return $token;
+    }
+
+    private function createUser($accessToken)
+    {
+        try {
+            $token = $this->parseToken($accessToken);
+            $claims = $token->claims();
+            $id = (int) $claims->get('sub');
+            $user = $this->userRepository->find($id);
+            if (!$user) {
+                $this->userRepository->createUserFromId($id);
+                return true;
+            }
+        } catch (Exception $e) {
+            $this->createUser($accessToken);
         }
     }
 }
