7d42c7c6617256002c8b940ca712041e954a7149
Nicolas H. Kosciuk <nhk@linkedcode.com>
2025-02-07 23:00:09 -0300

    CORS

--- src/Middleware/CorsMiddleware.php
+++ src/Middleware/CorsMiddleware.php
@@ -28,7 +28,7 @@ class CorsMiddleware implements MiddlewareInterface
         $response = $response
             ->withHeader('Access-Control-Allow-Credentials', 'true')
             ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
             ->withHeader('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
             ->withHeader('Pragma', 'no-cache');
